f7171e6a66907055d127ca611257fef8e0d38fe3
Abhishek Hulage <abhihulage14@gmail.com>
2024-06-19 19:18:53 +0530

    variables and datatypes in js

---
+++ index.js
@@ -0,0 +1,53 @@
+// Variables :
+
+// var : Variable can be redeclare and upadate
+var age = 20;
+var age = 30; // variable gets updated and also redeclared
+
+// let : Variables cannot be redeclare but can be update
+let age = 20;
+age = 30; // varible gets updated but didn't redclared
+
+// const: Variables cannot be redeclare and update
+const age = 20; // cannot be redeclared or updated or will give error
+
+
+/* -------------------------------------------------------------------------------------------------------------- */
+
+// Data Types :
+
+// Primitive Datatypes :
+
+// 1) number : any numeric value eg. integer, float
+let price = 100;
+
+// 2) String : text 
+let name = "Abhishek";
+
+// 3) Boolean : true or false
+let isAllowed = true;
+let isFollow = false;
+
+// 4) Undefined : when value is not assigned to variable then it is undefined
+let a; // it will print undefined
+
+// 5) Null : null is type of object : absense of value
+let b = null;
+
+// 6) BigInt : stores large integers
+let i = BigInt("23443523653467364"); // it will add n after the integer number
+
+// 7 Symbol : stores symbols
+let s = Symbol("Hello!"); // have type of symbol
+
+
+
+// Non-Primitive data types
+
+// Objects : Arrays, functions : objects are collection of value or peice of code which performs some functionality
+// objects have data with key and value
+
+const student = {
+    name: "Abhishek",
+    age: 22,
+}
